e4e3cce9e20c30b4df47e9255dd768219807d874
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-11 20:27:55 +0200

    switch5_4

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
-const inMoney = 12000;
-const yearPercent = 0.07;
-const housePrice = 13500;
-const year = 2;
-const amountMonth = year * 12;
+const role = 'admin';
+switch (role) {
+    case 'manager':
+        console.log('Менеджер');
+        break;
+    case 'admin':
+        console.log('Админ');
+        break;
+    case 'ceo':
+        console.log('CEO');
+        break;
+    default:
+        console.log('Мы тебя не знаем!');
+}
 
-const finish = inMoney * (1 + yearPercent / 12) ** amountMonth;
-if(finish>housePrice) {
-    const result = `Покупка успешна))Мы накопили ${finish}$
-    Остаток после покупки ${finish-housePrice}$`;
-    console.log(result);
-} else {
-    const result = `Покупка не удалась((
-    Мы накопили ${finish}$
-    не хватает ${housePrice-finish}$`;
-    console.log(result);
+switch (role) {
+    case 'manager':
+    case 'admin':
+        console.log('Не руководитель');
+        break;
+    case 'ceo':
+        console.log('Руководитель');
+        break;
+    default:
+        console.log('Мы тебя не знаем!');
+}  
+
+const num = 1;
+switch (true) {
+    case num > 0:
+        console.log('Положительный')
+        break;
+    case num < 0:
+        console.log('Отрицательный')
+        break;
+    default:
+        console.log('Ноль!')
 }
