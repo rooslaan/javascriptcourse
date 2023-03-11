73899e643294919a605796f6fc3309b8a0d1f8c0
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-11 20:06:07 +0200

    Task5_2

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-const money = 10;
-const price = 50;
+const inMoney = 12000;
+const yearPercent = 0.07;
+const housePrice = 13500;
+const year = 2;
+const amountMonth = year * 12;
 
-if(money > price) {
-    console.log('Может купить наш продукт');
-} else if (money > 5) {
-    console.log('Может купить наш mini продукт');
+const finish = inMoney * (1 + yearPercent / 12) ** amountMonth;
+if(finish>housePrice) {
+    const result = `Покупка успешна))Мы накопили ${finish}$
+    Остаток после покупки ${finish-housePrice}$`;
+    console.log(result);
 } else {
-    console.log('He может купить наш продукт');
+    const result = `Покупка не удалась((
+    Мы накопили ${finish}$
+    не хватает ${housePrice-finish}$`;
+    console.log(result);
 }
