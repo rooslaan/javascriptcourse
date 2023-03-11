3ecf4c46c4b975a0193d0a7debc478e07c8fca57
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-11 18:59:31 +0200

    Task4_7

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
-let salaryPerHour = 80;
-let time = 40;
-let fullTime = 9;
-let daysForTask = 40 / 5;
-console.log(daysForTask<fullTime);
+const projectName = 'Caйт магазина';
+const price = 2000;
+const author = 'Василий П.П.'
 
-console.log(time * salaryPerHour + '$');
+
+const template = author + ' заказал ' + projectName + ' по цене ' + price + '$';
+console.log(template);
+
+const template2= `${author} заказал ${projectName} по цене ${price}$`;
+console.log(template2);
+
+const template3 = 'Проэкт \n' + 'цена: ' + price + '$';
+console.log(template3);
+
+const template4 = `Проект
+Цена: ${price}$`;
+console.log(template4);
