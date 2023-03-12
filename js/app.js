111f8c020e4116b549769887565f14fdf3e6c5a5
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-12 16:05:31 +0200

    Task6_2

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-const answr = prompt('Сколько будет 7 + или - 15?');
+const isAdmin = true;
+const canWrite = true;
 
-switch(true) {
-    case Number(answr) === 22 :
-    case Number(answr) === -8 :
-    case answr === 'Я не робот' :
-        console.log('Успех!');
-        break;
-    default:
-        console.log('Вы робот!');
-}
+console.log(`Системный файл ${isAdmin && canWrite}`);
+console.log(`Обычный файл ${isAdmin || canWrite}`);
+console.log(`Инвертируем права админа ${!isAdmin}`);
+
+const isEdited = true;
+
+console.log(`Системный файл с редактированием 
+${isAdmin && canWrite && !isEdited}`);
