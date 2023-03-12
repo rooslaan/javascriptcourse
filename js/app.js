d02901e050171dcbc6aacd65154b98463efc2294
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-12 20:06:20 +0200

    Task7_1

--- js/app.js
+++ js/app.js
@@ -1,27 +1,15 @@
-/*
-let balance = 100;
-let bonusBalance = 90;
-let isBanned = true;
-let isExist = true;
-let isSelling = true;
-
-const a = balance >= 1000 && isBanned && isExist && isSelling;
-const b = bonusBalance >= 100 && isBanned && isExist && isSelling;
-
-if ((a === true) || (b === true)) {
-    console.log('Покупка успешна!');
-} else {
-    console.log('Не достаточно средств!(');
+function logName (name, surname) {
+    console.log(`Мое имя ${name} ${surname}`);
 }
-*/
-const balance = 1000;
-const bonusBalance = 90;
-const isBanned = false;
-const isExist = false;
-const isSelling = true;
 
-const canBuy = (balance >= 1000) || (bonusBalance >= 100)
-&& !isBanned && !isExist && isSelling;
-console.log(`Могу купить игру: ${canBuy ? 'Да)' : 'Нет('}`)
+logName('Руслан', 'Сябро');
 
+function countDepositSum(depositInUSD, month, rate) {
+    return depositInUSD * (1 + rate / 12) ** month;
+}
+
+const example1 = countDepositSum(1000, 24, 0.12);
+console.log(example1);
+
+console.log(countDepositSum(1000, 48, 0.1));
 
