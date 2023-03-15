58a4861f52053d5e9c3c3f72d7f2ba75a8ff0e20
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-15 19:22:59 +0200

    Task8_6

--- js/app.js
+++ js/app.js
@@ -1,13 +1,25 @@
-const roles = ['user', 'admin', 'manager', 'admin'];
+const tasks = ['Задача 1', 'Задача 2', 'Задача 3'];
 
-const elIndex = roles.indexOf('admin');
-console.log(elIndex);
-const elIndex2 = roles.indexOf('superUser');
-console.log(elIndex2);
-
-if (roles.includes('admin')) {
-    console.log('Доступ есть!');
+function addEl(tasks, nameTask) {
+    tasks.push(nameTask);
+    return tasks;
 }
 
-console.log(roles.includes('admin'));
+function deleteEl(tasks, nameTask) {
+    if (tasks.indexOf(nameTask) >= 0) {
+        tasks.indexOf(nameTask);
+        tasks.splice(tasks.indexOf(nameTask), 1);
+        return tasks;
+    }
+    return 'Такого элемента нет!';
+}
 
+ function swapEl(tasks, nameTask) {
+    if (tasks.indexOf(nameTask) >= 0) {
+        tasks.splice(tasks.indexOf(nameTask), 1);
+        tasks.unshift(nameTask);
+        return tasks
+    }
+    return 'Такого элемента нет!';
+ }
+ console.log(deleteEl(tasks, 'Задача 4'));
