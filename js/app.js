a3dcf92a4306afb9926d7c9aa4f5143d0b1bca8b
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-16 12:57:01 +0200

    Task9_5

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
-// for(let i = 1; i < 5; i++) {
-//     console.log(`Цикл 1 - ${i}`);
-//     for(let j = 1; j < 5; j++) {
-//         console.log(`Цикл 2 - ${j}`);
-//     }
-// }
-
-const tasks = [[1, 'task1'], 
-                [2, 'task2']];
-
-for(let i = 0; i < tasks.length; i++) {
-    for(let j = 0; j < tasks[i].length; j++) {
-        console.log(tasks[i][j]);
-    }
+for (let i = 1; i < 5; i++) {
+    console.log(`Вывод - ${i}`);
 }
+
+
+let i = 1;
+while (i < 5) {
+    console.log(`Вывод - ${i}`);
+    i++;
+}
+
+const srr = [1, 4, 8, 7,];
+
+for (let i = 0; i < srr.length; i++) {
+    if (srr[i] > 5) {
+        break;
+    }
+    console.log(srr[i]);  
+}
+
+
+while (srr[i] <= 5 && srr.length) {
+    console.log(srr[i]);
+    i++;
+}
+
+let j = 0;
+do {
+    console.log(j);
+    j++;
+} while(j < 0)
