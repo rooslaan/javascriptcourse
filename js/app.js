198baf19abe02df55ce8cea34939b1ff381a6e5c
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-16 11:00:57 +0200

    Task9_2

--- js/app.js
+++ js/app.js
@@ -1,24 +1,18 @@
-const url = 'https://purpleschool.ru/course/javascript';
-/*
-const res = url.split('/');
-let res1 = res[0].split(':');
-console.log('Протокол - ' + res1[0]);
-console.log('Доменное имя - ' + res[2]);
-let res2 = res.splice(3, 2);
-console.log('Путь внутри сайта - ' + res2.join('/'));*/
+const tasks = ['Задача 1', 'Задача 2', 'Задача 3'];
 
-function getUrlData(url) {
-    const [protocol, _, hostName, ...path] = url.split('/');
-    if (protocol === 'https' || protocol === 'http') {
-        if (!hostName.includes('.')) {
-            return;
-        }
-        console.log([protocol, _, hostName, path]);
-        console.log('Протокол - ' + protocol.split(':')[0]);
-        console.log('Доменное имя - ' + hostName);
-        console.log('Путь внутри сайта - /' + path.join('/'));
+for(let i = 0; i < tasks.length; i++) {
+    if (tasks[i] === 'Задача 2') {
+        continue;
     }
-    
+    console.log(tasks[i]);
 }
 
-getUrlData(url);
+console.log('-------');
+
+for(let i = 0; i < tasks.length; i++) {
+    if (tasks[i] === 'Задача 2') {
+        break;
+    }
+    console.log(tasks[i]);
+}
+
