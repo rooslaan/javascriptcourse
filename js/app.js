f2df8e957683aa61b08b59ce2ea1334f07c33159
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-16 13:06:50 +0200

    Task9_6

--- js/app.js
+++ js/app.js
@@ -1,31 +1,16 @@
-for (let i = 1; i < 5; i++) {
-    console.log(`Вывод - ${i}`);
+
+
+const arr = [1, 4, 8, 7,];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);  
 }
 
 
-let i = 1;
-while (i < 5) {
-    console.log(`Вывод - ${i}`);
-    i++;
+for (let element of arr) {
+    console.log(element);
 }
 
-const srr = [1, 4, 8, 7,];
-
-for (let i = 0; i < srr.length; i++) {
-    if (srr[i] > 5) {
-        break;
-    }
-    console.log(srr[i]);  
+for (let index in arr) {
+    console.log(index);
 }
-
-
-while (srr[i] <= 5 && srr.length) {
-    console.log(srr[i]);
-    i++;
-}
-
-let j = 0;
-do {
-    console.log(j);
-    j++;
-} while(j < 0)
