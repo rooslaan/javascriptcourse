a23aa2c93484c9f8421b12316ec54cce3b5d3495
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-28 12:12:58 +0300

    Task11_5

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
-const prices = [[100, 200], 
-                [120, 100], 
-                [200, 350]];
+const operations = [100, -20, 7, -30, 50];
+let balanse = 0;
 
+for (let operation of operations) {
+    balanse += operation;
+}
+console.log(balanse);
 
-let pricesDifference = prices
-    .map((price) => price[1] - price[0])
-    .filter((price) => price > 0);
+const finalBalanse =  operations.reduce((acc, value) => {
+    return acc += value;
+}, 0); 
 
-console.log(pricesDifference);
+// 0 - acc = 0, value = 100
+// 1 - acc = 100, value = -20
+// 2 - acc = 80, value = 7
+// 3 - acc = 87, value = -30
+// 4 - acc = 57, value = 50
+// 5 - acc = 107
+
+const minElement =  operations.reduce((acc, value) => {
+    if (value > acc) {
+        return acc;
+    } else {
+        return value;
+    }
+}, 0); 
+console.log(minElement);
+
