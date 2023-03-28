987c4f37aa001da4ad385fc7e8a1353ae003d009
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-28 20:14:51 +0300

    Task12_3

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
-const array = [1, 2, 3, 4, 5];
+const userName = 'Vasya aka Terminator Pupkin';
 
-console.log(new Array(1, 2, 3, 4, 5));
-const arr2 = new Array(5);
-console.log(arr2);
-arr2.fill(1, 0, 3);
-arr2.fill(2, 3, 5);
-console.log(arr2);
-
-const arr3 = Array.from({ length: 5}, (cur, i) => i + 1);
-console.log(arr3);
+const firstName = userName.slice(0, userName.indexOf(' '));
+const secondName = userName.slice(userName.lastIndexOf(' '), userName.length);
+console.log(firstName + secondName);
