8c3abe1cef1e9ae163a3a570bc74f333280ac295
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-28 12:49:17 +0300

    Task11_7(index/findIndex)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-const arr = [1, 4, 4, 10];
+const arr = [2, 4, 4, 10];
 
-const averageValue = arr.reduce((acc, value) => {
-    return acc += value / arr.length;
-}, 0);
-console.log(averageValue);
+// let findEl;
+// for (let el of arr) {
+//     if (el > 5) {
+//         findEl = el;
+//         break;
+//     }
+// }
+// console.log(findEl);
+
+const findEl = arr.find(el => el > 5);
+console.log(findEl);
+
+const findIndexOfEl = arr.findIndex(el => el < 0);
+console.log(findIndexOfEl);
+
+
+
