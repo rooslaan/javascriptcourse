c6ff28ecc9c780dbb32a4d2965c9cc6a6d2e6d0a
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-28 20:17:04 +0300

    Task12_3(1)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const userName = 'Vasya aka Terminator Pupkin';
 
 const firstName = userName.slice(0, userName.indexOf(' '));
-const secondName = userName.slice(userName.lastIndexOf(' '), userName.length);
-console.log(firstName + secondName);
+const secondName = userName.slice(userName.lastIndexOf(' ') + 1, userName.length);
+console.log(firstName + ' ' + secondName);
