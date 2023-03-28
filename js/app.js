13ac7725f8421995fdd360c53ac10270977c2fd4
Siabro Ruslan <sbrruslanchik@gmail.com>
2023-03-28 12:35:40 +0300

    Task11_6

--- js/app.js
+++ js/app.js
@@ -1,28 +1,6 @@
-const operations = [100, -20, 7, -30, 50];
-let balanse = 0;
+const arr = [1, 4, 4, 10];
 
-for (let operation of operations) {
-    balanse += operation;
-}
-console.log(balanse);
-
-const finalBalanse =  operations.reduce((acc, value) => {
-    return acc += value;
-}, 0); 
-
-// 0 - acc = 0, value = 100
-// 1 - acc = 100, value = -20
-// 2 - acc = 80, value = 7
-// 3 - acc = 87, value = -30
-// 4 - acc = 57, value = 50
-// 5 - acc = 107
-
-const minElement =  operations.reduce((acc, value) => {
-    if (value > acc) {
-        return acc;
-    } else {
-        return value;
-    }
-}, 0); 
-console.log(minElement);
-
+const averageValue = arr.reduce((acc, value) => {
+    return acc += value / arr.length;
+}, 0);
+console.log(averageValue);
